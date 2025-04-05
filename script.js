b521f83de168704f365c363140a81206f6172ff4
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-04-05 17:50:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// your code here
+let inputName = document.querySelector('#name');
+let inputYear = document.querySelector('#year');
+let heading = document.querySelector('#url');
+heading.innerText = ' https://localhost:8080/'
+let btn = document.querySelector('#button');
+btn.addEventListener("click",()=>{
+	e.preventDefault();
+	let name = inputName.value;
+	let year = inputYear.value;
+
+	if(name && year){
+		heading.innerText = `https://localhost:8080/?name=${name}&year=${2022}`
+		return;
+	}
+	
+	if(!name){
+		heading.innerText = `https://localhost:8080/?year=${year}`
+		return;
+	}
+
+	if(!year){
+		heading.innerText = `https://localhost:8080/?name=${name}`
+		return;
+	}
+})
